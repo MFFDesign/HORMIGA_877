9bb93274c8c62c77bf626b1456f5333eca775a90
MFF Design <51934155+MFFDesign@users.noreply.github.com>
2023-02-07 16:12:59 -0600

    Se corrige PID

--- Hormiga_877_RevA.X/PIDController.c
+++ Hormiga_877_RevA.X/PIDController.c
@@ -6,6 +6,7 @@ struct Controller{
     double Kp;
     double Ki;
     double Kd;
+    double dtC;
     char SampleT;
     unsigned int TotalEvents;
     double Error;
@@ -18,12 +19,20 @@ struct Controller{
     double AccionControl;
 }PID;
 
-void PIDSetSampleTime(char dt)
+double map(double data, double X2, double X1, double Y2, double Y1)
+{
+    double m = (Y2 - Y1) / (X2 - X1);
+    return (m * data) - (m * X1) + Y1;
+}
+
+
+void SetSampleTime(char dt)
 {
     OPTION_REG &= 0xD7;
     TMR0IF = 0;
     TMR0 = 0xFF - dt;
     PID.SampleT = dt;
+    PID.dtC = PID.SampleT * 0.000051;
 }
 
 void SetProportionalTune(double value)
@@ -41,15 +50,15 @@ void SetDerivativeTune(double value)
     PID.Kd = value;
 }
 
-unsigned char AccionControl(double SetPoint, double Feedback, double dt)
+double AccionControl(double SetPoint, double Feedback)
 {
     PID.Error = SetPoint - Feedback;
-    PID.ErrorAcum += PID.Error;
+    PID.ErrorAcum += (PID.Error * PID.dtC) ;
     PID.P = PID.Kp * PID.Error;
     PID.I = PID.Ki * PID.ErrorAcum;
-    PID.D = PID.Kd * ((PID.Error - PID.LastError) / dt);
+    PID.D = PID.Kd * ((PID.Error - PID.LastError) / PID.dtC);
     PID.AccionControl = PID.P + PID.I + PID.D;
-    return (char)PID.AccionControl;
+    return PID.AccionControl;
 }
 
 unsigned int EventCounter(void)
