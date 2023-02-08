97bf599b2b036251103ec3225e77084535782d99
MFF Design <51934155+MFFDesign@users.noreply.github.com>
2023-02-08 16:00:24 -0600

    Se agregan funciones de retardo por HW

--- Hormiga_877_RevA.X/PIDController.c
+++ Hormiga_877_RevA.X/PIDController.c
@@ -6,7 +6,7 @@ struct Controller{
     double Kp;
     double Ki;
     double Kd;
-    double dtC;
+    double HWdt;
     char SampleT;
     unsigned int TotalEvents;
     double Error;
@@ -25,14 +25,25 @@ double map(double data, double X2, double X1, double Y2, double Y1)
     return (m * data) - (m * X1) + Y1;
 }
 
+void SetSampleTime(double Sample)
+{
+    PID.SampleT = Sample;
+}
 
-void SetSampleTime(char dt)
+void SetHWSampleTime(char dt)
 {
     OPTION_REG &= 0xD7;
     TMR0IF = 0;
     TMR0 = 0xFF - dt;
-    PID.SampleT = dt;
-    PID.dtC = PID.SampleT * 0.000051;
+    PID.HWdt = dt;
+}
+
+unsigned char HWDelay(void)
+{
+    TMR0IF = 0;
+    TMR0 = 0xFF - PID.HWdt;
+    while(!TMR0IF);
+    return 1;
 }
 
 void SetProportionalTune(double value)
@@ -53,10 +64,10 @@ void SetDerivativeTune(double value)
 double AccionControl(double SetPoint, double Feedback)
 {
     PID.Error = SetPoint - Feedback;
-    PID.ErrorAcum += (PID.Error * PID.dtC) ;
+    PID.ErrorAcum += (PID.Error * PID.SampleT) ;
     PID.P = PID.Kp * PID.Error;
     PID.I = PID.Ki * PID.ErrorAcum;
-    PID.D = PID.Kd * ((PID.Error - PID.LastError) / PID.dtC);
+    PID.D = PID.Kd * ((PID.Error - PID.LastError) / PID.SampleT);
     PID.AccionControl = PID.P + PID.I + PID.D;
     return PID.AccionControl;
 }
